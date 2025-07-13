b5ad47b251ee82a1ac0ca8cb37f68389e953cb70
Mohamed Gharieb <m.gharieb89@gmail.com>
2025-07-14 01:29:46 +0300

    file 6 ver2

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
-income = int(input("Enter your monthly income: "))
-expences = int(input("Enter your total monthly expenses: "))
+income = float(input("Enter your monthly income: "))
+expences = float(input("Enter your total monthly expenses: "))
 savings = income - expences
 interest_rate = 0.05
 total_saving = (savings * 12) + (savings * 12 * interest_rate)
