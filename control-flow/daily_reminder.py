a795f2edf1d52009a89eb3bfc44e753333b89cdb
Mohamed Gharieb <m.gharieb89@gmail.com>
2025-07-19 23:41:12 +0300

    t4v0

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,20 @@
+# Create a simplified Python script that uses conditional statements, Match Case, and loops to remind the user about a single, priority task for the day based on time sensitivity.
+task = input("Enter your task: ")
+priority = input("priority (high/medium/low): ").lower()
+time_bond = input("Is it time-bound? (yes/no): ").lower()
+match priority:
+    case "high":
+        if time_bond == "yes":
+            print("Reminder: ", task,"is a high priority task that requires immediate attention today!")
+        else:
+            print("Reminder: ", task,"is a high priority task that requires attention today!")
+    case "meduim":
+        if time_bond == "yes":
+            print("Alert: ", task,"is a meduim priority task that requires attention today!")
+        else:
+            print("Alert: ", task,"is a meduim priority task that requires attention today!")
+    case "low":
+        if time_bond == "yes":
+            print("warrning: ", task,"is a low priority task that requires attention today!")
+        else:
+            print("Warrning: ", task,"is a low priority task that requires attention today!")
