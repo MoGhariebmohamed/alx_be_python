23dd69d719ad6102aa610e9ad1942196aa70cf20
Mohamed Gharieb <m.gharieb89@gmail.com>
2025-07-19 23:54:10 +0300

    t4v2

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,17 +4,17 @@ priority = input("priority (high/medium/low): ").lower()
 time_bond = input("Is it time-bound? (yes/no): ").lower()
 match priority:
     case "high":
-        if time_bond == "yes":
+        if time_bond=="yes":
             print("Reminder: ", task,"is a high priority task that requires immediate attention today!")
         else:
             print("Reminder: ", task,"is a high priority task that requires attention today!")
     case "meduim":
-        if time_bond == "yes":
+        if time_bond=="yes":
             print("Alert: ", task,"is a meduim priority task that requires attention today!")
         else:
             print("Alert: ", task,"is a meduim priority task that requires attention today!")
     case "low":
-        if time_bond == "yes":
+        if time_bond=="yes":
             print("warrning: ", task,"is a low priority task that requires attention today!")
         else:
             print("Warrning: ", task,"is a low priority task that requires attention today!")
